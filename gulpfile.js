410398ec601ec9b166ea4b7a0e934729bc44a73f
jijia <756951628@qq.com>
2022-03-14 17:07:26 +0800

    build: 添加通过gulp快速生成leetcode测试文件
    - gulpfile添加readFileTemplate方法

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const { src, dest } = require("gulp");
 const rename = require("gulp-rename");
 
+const { Transform } = require("stream");
+const { basename } = require("path");
+
 /**
  * 创建时间文件名
  * @returns string
@@ -47,5 +50,64 @@ function leetcodeSave(done) {
     }
 }
 
+/**
+ * 读取模板替换模板参数
+ * @param {object} option
+ * @param {object} option.params 替换参数
+ * @returns
+ */
+function readFileTemplate(option) {
+    const replaceParams = option.params;
+
+    const transform = new Transform({
+        objectMode: true,
+        transform: function (chunk, enc, cb) {
+            let file = chunk.contents.toString();
+
+            for (const key in replaceParams) {
+                const reg = new RegExp(`\\$\\#\\{${key}\\}\\#`, "g");
+                file = file.replace(reg, replaceParams[key]);
+            }
+
+            const b = Buffer.from(file);
+
+            chunk.contents = b;
+
+            cb(null, chunk);
+        },
+    });
+
+    return transform;
+}
+
+function createLeetcodeTestOnJest(done) {
+    const reg = /^leetcode\//;
+    const file = process.env.file.replace(/\\/g, "/");
+    const bName = basename(file, ".js");
+    console.log(bName);
+
+    if (reg.test(file)) {
+        return src("template/leetcodeTestOnJest.template.js")
+            .pipe(
+                readFileTemplate({
+                    params: { title: bName },
+                })
+            )
+            .pipe(
+                rename(function (path) {
+                    return {
+                        dirname: "leetcode/",
+                        basename: bName,
+                        extname: ".spec.js",
+                    };
+                })
+            )
+            .pipe(dest("./tests"));
+    } else {
+        done();
+    }
+}
+
 exports.default = leetcodeSave;
 exports.leetcode = leetcodeSave;
+exports.createLeetcodeTest = createLeetcodeTestOnJest;
---
+++ template/leetcodeTestOnJest.template.js
@@ -0,0 +1,5 @@
+import {} from "../../leetcode/$#{title}#.js";
+
+describe("leetCode测试用例", () => {});
+
+describe("自定义", () => {});
