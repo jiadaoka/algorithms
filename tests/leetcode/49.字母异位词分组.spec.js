472e12a7a6c1e3add658d6a5b1718a16c0c1c43e
jijia <756951628@qq.com>
2022-03-31 17:35:06 +0800

    perf: leetcode题目
    - 49.字母异位词分组

--- tests/leetcode/49.字母异位词分组.spec.js
+++ tests/leetcode/49.字母异位词分组.spec.js
@@ -1,25 +1,25 @@
 import { groupAnagrams, isEqualMap } from "../../leetcode/49.字母异位词分组.js";
 
 describe("leetCode测试用例", () => {
-    // test("用例1", () => {
-    //     const expectedAnswer = expect.arrayContaining([
-    //         ["bat"],
-    //         expect.arrayContaining(["nat", "tan"]),
-    //         expect.arrayContaining(["ate", "eat", "tea"]),
-    //     ]);
+    test("用例1", () => {
+        const expectedAnswer = expect.arrayContaining([
+            ["bat"],
+            expect.arrayContaining(["nat", "tan"]),
+            expect.arrayContaining(["ate", "eat", "tea"]),
+        ]);
 
-    //     const output = groupAnagrams([
-    //         "eat",
-    //         "tea",
-    //         "tan",
-    //         "ate",
-    //         "nat",
-    //         "bat",
-    //     ]);
+        const output = groupAnagrams([
+            "eat",
+            "tea",
+            "tan",
+            "ate",
+            "nat",
+            "bat",
+        ]);
 
-    //     expect(output).toStrictEqual(expectedAnswer);
-    //     expect(output).toHaveLength(3);
-    // });
+        expect(output).toStrictEqual(expectedAnswer);
+        expect(output).toHaveLength(3);
+    });
 
     test("用例2", () => {
         const expectedAnswer = [[""]];
@@ -42,33 +42,33 @@ describe("leetCode测试用例", () => {
 
 // describe("自定义", () => {});
 
-describe("Map是否相同", () => {
-    test("用例1", () => {
-        const m = new Map();
-        m.set(1, 1);
+// describe("Map是否相同", () => {
+//     test("用例1", () => {
+//         const m = new Map();
+//         m.set(1, 1);
 
-        const m2 = m;
+//         const m2 = m;
 
-        expect(isEqualMap(m, m2)).toBe(true);
-    });
+//         expect(isEqualMap(m, m2)).toBe(true);
+//     });
 
-    test("用例2", () => {
-        const m = new Map();
-        m.set(1, 1);
+//     test("用例2", () => {
+//         const m = new Map();
+//         m.set(1, 1);
 
-        const m2 = new Map();
-        m2.set(1, 1);
+//         const m2 = new Map();
+//         m2.set(1, 1);
 
-        expect(isEqualMap(m, m2)).toBe(true);
-    });
+//         expect(isEqualMap(m, m2)).toBe(true);
+//     });
 
-    test("用例3", () => {
-        const m = new Map();
-        m.set(1, 1);
+//     test("用例3", () => {
+//         const m = new Map();
+//         m.set(1, 1);
 
-        const m2 = new Map();
-        m2.set(2, 1);
+//         const m2 = new Map();
+//         m2.set(2, 1);
 
-        expect(isEqualMap(m, m2)).toBe(false);
-    });
-});
+//         expect(isEqualMap(m, m2)).toBe(false);
+//     });
+// });
